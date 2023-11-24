4f1955d7acb76377e57c380e0d496e442ea68a9a
AbdullahAlHamourAlJarad <aalhamou@uwo.ca>
2023-11-23 20:47:39 -0500

    Set up .env

--- data-diver-backend/src/index.ts
+++ data-diver-backend/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Kareem Is The Best!');
