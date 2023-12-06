7d42370b59b58bc20800a0807fe687088db300e8
AbdullahAlHamourAlJarad <aalhamou@uwo.ca>
2023-12-06 02:53:35 -0500

    Created All SQL Tables and Insert Data

--- data-diver-backend/sql/creatTxn.sql
+++ data-diver-backend/sql/creatTxn.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Transactions (
     transaction_id INT PRIMARY KEY,
     customer_id INT FOREIGN KEY REFERENCES Customers(customer_id),
-    Amount DECIMAL(10, 2), -- Adjust precision and scale as needed
-    merchant VARCHAR(255), -- Adjust the length as needed
+    Amount DECIMAL(10, 2), 
+    merchant VARCHAR(255), 
     attempts INT,
     approved VARCHAR(3) CHECK (approved IN ('Yes', 'No')),
-    fraud DECIMAL(10, 2) -- Adjust precision and scale as needed
+    fraud DECIMAL(10, 2) 
 );
