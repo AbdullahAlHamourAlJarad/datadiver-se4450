d5cb3ec6b06ac7b6989e60a36823005f06044aa0
Kareem <kmeawad@uwo.ca>
2024-02-20 22:11:10 -0500

    Add Header to all routes/pages

--- data-diver-frontend/src/App.tsx
+++ data-diver-frontend/src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ErrorBoundary } from 'react-error-boundary';
 import DataDiver from './components/DataDiver';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, Grid, ThemeProvider, createTheme } from '@mui/material';
 import PrivateRoute from './PrivateRoute';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Header from './components/Header';
 
 const theme = createTheme({});
 
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const router = createBrowserRouter([
-    { path: "/", element: <PrivateRoute element={DataDiver}/> },
+    { path: "/", element: <PrivateRoute element={DataDiver} drawerOpen={drawerOpen}/> },
     { path: "/login", element: <Login /> },
     { path: "/signup", element: <Signup />}
   ]);
@@ -21,7 +23,12 @@ function App() {
     <ErrorBoundary fallback={<div>Error Occurred</div>}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <RouterProvider router={router} />
+            <Grid container>
+              <Grid item xs={12}>
+                  <Header setDrawerOpen={setDrawerOpen} />
+              </Grid>
+              <RouterProvider router={router} />
+            </Grid>
       </ThemeProvider>
     </ErrorBoundary>
   );
--- data-diver-frontend/src/components/DataDiver.tsx
+++ data-diver-frontend/src/components/DataDiver.tsx
@@ -1,28 +1,27 @@
 import React, { useState } from 'react'
 import { Grid } from '@mui/material';
-import Header from './Header';
 import Chat from './Chat';
 import Drawer from './Drawer';
 
-function DataDiver() {
-    const [drawerOpen, setDrawerOpen] = useState(false)
+type DataDiverProps = {
+    drawerOpen: boolean
+}
+
+function DataDiver({drawerOpen} : Readonly<DataDiverProps>) {
     const [dbURL, setDbURL] = useState('datadiverserver.database.windows.net')
     const [dbName, setDbName] = useState('DataDiverDB')
     const [dbUsername, setDbUsername] = useState('datadiveradmin')
     const [dbPassword, setDbPassword] = useState('ouda2023!')
 
     return (
-        <Grid container>
-            <Grid item sx={{ height: '10vh' }} xs={12}>
-                <Header drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
-            </Grid>
+        <>
             <Grid item sx={{ height: '90vh' }} xs={drawerOpen ? 9 : 12}>
                 <Chat dbURL={dbURL} dbName={dbName} dbUsername={dbUsername} dbPassword={dbPassword} />
             </Grid>
             <Grid item sx={{ height: '90vh' }} xs={3} hidden={!drawerOpen}>
                 <Drawer dbURL={dbURL} dbName={dbName} dbUsername={dbUsername} dbPassword={dbPassword} setDbURL={setDbURL} setDbName={setDbName} setDbUsername={setDbUsername} setDbPassword={setDbPassword}/>
             </Grid>
-        </Grid>
+        </>
     );
 }
 
